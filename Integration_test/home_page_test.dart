be6357d5ced3ff3949ed2e604942ea204dd6c061
Terbish <terb.avg@gmail.com>
2024-05-13 23:52:26 -0700

    - added integration testing

---
+++ Integration_test/home_page_test.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:group_escape/firebase_options.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:group_escape/pages/home_page.dart';
+import 'package:group_escape/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  setUpAll(() async{
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  });
+
+  Future<void> signIn(WidgetTester tester) async {
+    // Sign in with test credentials
+    final Finder emailField = find.widgetWithText(TextField, 'Email');
+    await tester.enterText(emailField, 'jerry.test@gmail.com');
+    await tester.pumpAndSettle();
+
+    final Finder passwordField = find.widgetWithText(TextField, 'Password');
+    await tester.enterText(passwordField, 'mcspar4893');
+    await tester.pumpAndSettle();
+
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pumpAndSettle();
+
+    final Finder loginButton = find.widgetWithText(ElevatedButton, "Log in");
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> homePage(WidgetTester tester) async {
+    expect(find.text('My Trip'), findsOne);
+  }
+
+  group('HomePage Integration Tests', () {
+    testWidgets('Check if the HomePage widget is rendered', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      await signIn(tester);
+      await tester.pumpAndSettle();
+
+      await homePage(tester);
+
+    });
+  });
+}
--- test/create_trip_test.dart
+++ test/create_trip_test.dart
@@ -1,15 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:group_escape/pages/create_trip.dart';
 import 'package:group_escape/services/firestore_service.dart';
 import 'package:group_escape/shared/firebase_authentication.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-@GenerateMocks([Firebase])
+@GenerateMocks([FirestoreService])
 void main() {
-  group('Create Trip Test', (){
-    testWidgets('CreateTrip widget renders correctly', (WidgetTester tester) async {
-
+  group('Create Trip Test', () {
+    testWidgets('CreateTrip widget renders correctly',
+        (WidgetTester tester) async {
+      final mockFirestore = MockFirestoreService();
       await tester.pumpWidget(
         const MaterialApp(
           home: CreateTrip(),
@@ -17,10 +20,18 @@ void main() {
       );
 
       expect(find.byType(AppBar), findsOneWidget);
-      expect(find.descendant(of: find.byType(AppBar), matching: find.text('Create Trip')), findsOneWidget);
+      expect(
+          find.descendant(
+              of: find.byType(AppBar), matching: find.text('Create Trip')),
+          findsOneWidget);
       expect(find.byType(TextFormField), findsNWidgets(4));
       expect(find.byType(ElevatedButton), findsOneWidget);
-      expect(find.descendant(of: find.byType(ElevatedButton), matching: find.text('Create Trip')), findsOneWidget);
+      expect(
+          find.descendant(
+              of: find.byType(ElevatedButton),
+              matching: find.text('Create Trip')),
+          findsOneWidget);
     });
+
   });
 }
