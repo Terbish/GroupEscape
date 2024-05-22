09d681aefcdf91ed7df44b2874bf050d51c8e8a2
JuliGH6 <julianwoern@googlemail.com>
2024-05-21 17:17:40 -0700

    Added notifications and adjusted tested for firestore service and join trip dialog

--- lib/widgets/join_trip_dialog.dart
+++ lib/widgets/join_trip_dialog.dart
@@ -20,8 +20,8 @@ class JoinTripDialog extends StatelessWidget {
     } else {
       await firestoreService.addUserToTrip(
           _controller.text, authInstance.currentUser());
-      // await firestoreService.subscribeToTopic(_controller.text);
       await firestoreService.sendNotification(topic: _controller.text);
+      await firestoreService.subscribeToTopic(_controller.text);
       Navigator.pop(context, _controller.text);
     }
   }
